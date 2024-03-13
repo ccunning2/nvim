3ff03ebbde33fc23a39c55cfa7f22a0e0c3c54de
Cameron Cunning <negatifzeo@gmail.com>
2024-03-12 20:10:48 -0500

    more

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,13 @@
+return {
+  --This allows some linters/formatters to use LSP integration
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+			},
+		})
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,8 @@ vim.g.mapleader = " "
 
 --Use MAC clipboard
 vim.cmd("set clipboard=unnamedplus")
+
+
+--Line numbers
+vim.cmd("set number")
+vim.cmd("set relativenumber")
