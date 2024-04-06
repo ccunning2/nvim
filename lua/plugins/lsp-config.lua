f193982900db5a1d5051d110e3606677481e3133
Cameron Cunning <negatifzeo@gmail.com>
2024-04-06 09:18:23 -0500

    Include rustaceanvim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -26,7 +26,7 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+      --      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
